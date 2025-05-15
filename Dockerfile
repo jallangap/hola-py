f7f0aaf8efff7addeef062f35709556189d4f153
Johan Llanga <andijohan9393@hotmail.com>
2025-05-15 07:20:59 -0500

    Cambios en el proyecto

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM python:3.9
+
 WORKDIR /app
-COPY hola.py /app/hola.py
+
+COPY ..
+
+RUN pip install -r requirements.txt 
+
+EXPOSE 8080
+
 CMD ["python", "hola.py"]
--- hola.py
+++ hola.py
@@ -1,3 +1,12 @@
-print("Hola, mundo")
-print("Railway")
-print("valen verga")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hola"
+
+
+if __name__=="__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
